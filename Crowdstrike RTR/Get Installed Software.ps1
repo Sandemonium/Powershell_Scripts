ca57688f8c24fcf353f0db196f05003aac9e9de5
Sandemonium <80851348+Sandemonium@users.noreply.github.com>
2023-03-01 15:04:26 -0800

    Formatting cleanup

--- Crowdstrike RTR/Get Installed Software.ps1
+++ Crowdstrike RTR/Get Installed Software.ps1
@@ -3,13 +3,12 @@
 
 function grk ([string]$Str) {
 	#Create a new object getting each hive from the registry
-    $Obj = foreach ($N in (Get-ChildItem 'Registry::\').PSChildName) {
-        #If the hive is HKEY_USERS, Get each user hive, using regex to include only user SIDs, excluding _CLASSES.
+	$Obj = foreach ($N in (Get-ChildItem 'Registry::\').PSChildName) {
+		#If the hive is HKEY_USERS, Get each user hive, using regex to include only user SIDs, excluding _CLASSES.
 		if ($N -eq 'HKEY_USERS') {
-            foreach ($V in (Get-ChildItem "Registry::\$N" -EA 0 | Where-Object { $_.Name -match
-            'S-\d-\d+-(\d+-){1,14}\d+$' }).PSChildName) {
-				#for each user hive, test if the supplied '$str' parameter (path past the user SID, i.e. 'Software\Microsoft\Windows\CurrentVersion\Uninstall') exists, if so, get all child objects.
-                if (Test-Path "Registry::\$N\$V\$Str") { Get-ChildItem "Registry::\$N\$V\$Str" -EA 0 }
+		foreach ($V in (Get-ChildItem "Registry::\$N" -EA 0 | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' }).PSChildName) {
+			#for each user hive, test if the supplied '$str' parameter (path past the user SID, i.e. 'Software\Microsoft\Windows\CurrentVersion\Uninstall') exists, if so, get all child objects.
+			if (Test-Path "Registry::\$N\$V\$Str") { Get-ChildItem "Registry::\$N\$V\$Str" -EA 0 }
             }
 		#Else if it's not a user hive, check after the hive for the supplied path in the $Str parameter. If it exists, get child objects.
         } elseif (Test-Path "Registry::\$N\$Str") {
@@ -22,7 +21,7 @@ function grk ([string]$Str) {
 		#Add the registry path for the install key to help with removal.
 		$I | Add-Member -Name "RegPath" -Value "$($_.name)" -MemberType NoteProperty -Force
         foreach ($P in $_.Property) {
-            $I.PSObject.Properties.Add((New-Object PSNoteProperty($P,($_.GetValue($P)))))
+        	$I.PSObject.Properties.Add((New-Object PSNoteProperty($P,($_.GetValue($P)))))
         }
         $I
     }
@@ -30,8 +29,7 @@ function grk ([string]$Str) {
 #Run the grk function using Software\(remaining path) to check two places for install keys, filtering for objects that have a DisplayName, DisplayVersion, and Publisher, then selecting relevant properties.
 $Out = @('Microsoft\Windows\CurrentVersion\Uninstall',
 'Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall').foreach{
-    grk "Software\$_" | Where-Object { $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } |
-        Select-Object DisplayName,DisplayVersion,Publisher,InstallLocation,QuietUninstallString,UninstallString,RegPath
+    grk "Software\$_" | Where-Object { $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } | Select-Object DisplayName,DisplayVersion,Publisher,InstallLocation,QuietUninstallString,UninstallString,RegPath
 }
 #Display the output and sort by the displayname.
 $Out | sort displayname
