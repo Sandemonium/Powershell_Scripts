34a46f2d29060f923f45f996edd73e07dd845410
Sandemonium <80851348+Sandemonium@users.noreply.github.com>
2023-03-01 15:55:00 -0800

    Renames and formatting corrections to all files.

--- Crowdstrike_RTR/Get-Installed_Software.ps1
+++ Crowdstrike_RTR/Get-Installed_Software.ps1
@@ -9,27 +9,27 @@ function grk ([string]$Str) {
 		foreach ($V in (Get-ChildItem "Registry::\$N" -EA 0 | Where-Object { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' }).PSChildName) {
 			#for each user hive, test if the supplied '$str' parameter (path past the user SID, i.e. 'Software\Microsoft\Windows\CurrentVersion\Uninstall') exists, if so, get all child objects.
 			if (Test-Path "Registry::\$N\$V\$Str") { Get-ChildItem "Registry::\$N\$V\$Str" -EA 0 }
-            }
+			}
 		#Else if it's not a user hive, check after the hive for the supplied path in the $Str parameter. If it exists, get child objects.
-        } elseif (Test-Path "Registry::\$N\$Str") {
-            Get-ChildItem "Registry::\$N\$Str" -EA 0
-        }
-    }
+		} elseif (Test-Path "Registry::\$N\$Str") {
+			Get-ChildItem "Registry::\$N\$Str" -EA 0
+		}
+	}
 	#For each found key, create a custom object called $I and add all properties to it, then output $I
-    $Obj | ForEach-Object {
-        $I = [PSCustomObject]@{}
+	$Obj | ForEach-Object {
+		$I = [PSCustomObject]@{}
 		#Add the registry path for the install key to help with removal.
 		$I | Add-Member -Name "RegPath" -Value "$($_.name)" -MemberType NoteProperty -Force
-        foreach ($P in $_.Property) {
-        	$I.PSObject.Properties.Add((New-Object PSNoteProperty($P,($_.GetValue($P)))))
-        }
-        $I
-    }
+		foreach ($P in $_.Property) {
+			$I.PSObject.Properties.Add((New-Object PSNoteProperty($P,($_.GetValue($P)))))
+		}
+		$I
+	}
 }
 #Run the grk function using Software\(remaining path) to check two places for install keys, filtering for objects that have a DisplayName, DisplayVersion, and Publisher, then selecting relevant properties.
 $Out = @('Microsoft\Windows\CurrentVersion\Uninstall',
 'Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall').foreach{
-    grk "Software\$_" | Where-Object { $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } | Select-Object DisplayName,DisplayVersion,Publisher,InstallLocation,QuietUninstallString,UninstallString,RegPath
+	grk "Software\$_" | Where-Object { $_.DisplayName -and $_.DisplayVersion -and $_.Publisher } | Select-Object DisplayName,DisplayVersion,Publisher,InstallLocation,QuietUninstallString,UninstallString,RegPath
 }
 #Display the output and sort by the displayname.  Optionally, append '| ft -auto' to this line to format as a table in PS.
 $Out | sort displayname
